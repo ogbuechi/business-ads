82cd71b8a0d2ddb23388648054c04d5aa26f815a
Benjamin Chukwudi <benjaminchukwudi0@gmail.com>
2019-05-14 07:59:53 +0100

    csrf

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" type="text/css" href="/assets/css/owl.carousel.css">
 
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css">
 
